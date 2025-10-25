75cc2224c22aad130034601cc6ba519d36aa5e57
bkochman5 <bkochman5@gmail.com>
2025-10-25 17:00:06 +0000

    Add Redis and Docker Compose

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
 
+db = Redis(host='db', port=6379, decode_responses=True)
+
 @app.route('/')
 def hello():
-    return "Hello from my Docker Container! 🐳"
+    count = db.incr('hits')
+    return f"Hello from Docker! 🐳 This page has been visited {count} times."
 
 if __name__ == '__main__':
     #0.0.0.0 so server is available outside the container
