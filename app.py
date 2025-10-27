3e81acf4f09b65adcfcbfb64970b21220908ee60
bkochman5 <bkochman5@gmail.com>
2025-10-27 18:14:30 +0000

    Update app to v2.0

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+# app.py
 from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-
 db = Redis(host='db', port=6379, decode_responses=True)
 
 @app.route('/')
 def hello():
     count = db.incr('hits')
-    return f"Hello from Docker! 🐳 This page has been visited {count} times."
+    # --- THIS IS THE LINE TO CHANGE ---
+    return f"Hello from v2.0! 🚀 This page has been visited {count} times."
 
 if __name__ == '__main__':
-    #0.0.0.0 so server is available outside the container
     app.run(host='0.0.0.0', port=5000)
